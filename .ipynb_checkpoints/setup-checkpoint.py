a1777b248c08b71e45bb5120f4107e661ac78322
yiling-h <yilingh@umich.edu>
2025-03-18 16:36:44 -0400

    Setup.py

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="selectinf",
+    version="0.1",
+    packages=find_packages(),  # Automatically finds all packages under 'selectinf/'
+)
